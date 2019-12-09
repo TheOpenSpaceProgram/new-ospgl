037a7a3f24fb0b699767c6d517edf8cd6db4b707
Tatjam <tajampi@gmail.com>
2019-12-09 01:55:47 +0100

    Destroying of welds seems to be correctly working thanks to the awesome help from Capital-Asterisk!
    Former-commit-id: 75fa64658e23c85e859f258b6b758bcfb6c6e9a5 [formerly babb9149fa15f45a629558bf81bbf1b209b85551 [formerly 378d64af1b2e4096f11e14d48af4e7803e4d559e]]
    Former-commit-id: 917c16f7b11184db1162bd8a8478a1140abaa4ef
    Former-commit-id: bf49b6cba45a29a0db28311532e42934530b9c54

--- src/physics/glm/BulletGlmCompat.h
+++ src/physics/glm/BulletGlmCompat.h
@@ -14,8 +14,25 @@ inline glm::vec3 to_vec3(const btVector3 data)
 	return glm::vec3((float)data.x(), (float)data.y(), (float)data.z());
 }
 
+inline glm::dquat to_dquat(const btQuaternion data)
+{
+	return glm::dquat((double)data.w(), (double)data.x(), (double)data.y(), (double)data.z());
+}
+
+inline glm::dmat4 to_dmat4(const btTransform data)
+{
+	glm::dvec3 off = to_dvec3(data.getOrigin());
+	glm::dquat rot = to_dquat(data.getRotation());
+
+	glm::dmat4 mat = glm::translate(glm::dmat4(1.0), off);
+	mat = glm::toMat4(rot) * mat;
+
+	return mat;
+}
+
 template<typename T>
 inline btVector3 to_btVector3(glm::tvec3<T> d)
 {
 	return btVector3((btScalar)d.x, (btScalar)d.y, (btScalar)d.z);
 }
+
--- src/vehicle/part/Piece.cpp
+++ src/vehicle/part/Piece.cpp
@@ -2,7 +2,7 @@
 
 
 
-btTransform Piece::get_current_position()
+btTransform Piece::get_current_transform()
 {
 	if (motion_state == nullptr)
 	{
@@ -12,19 +12,63 @@ btTransform Piece::get_current_position()
 	{
 		btTransform tform;
 
+
 		//motion_state->getWorldTransform(tform);
 
 		tform = rigid_body->getWorldTransform();
 
 		if (in_group != nullptr)
 		{
-			return welded_tform * tform;
+			return tform * welded_tform;
 		}
 		else
 		{
 			return tform;
 		}
 	}
+}
+
+btVector3 Piece::get_current_linear()
+{
+	if (rigid_body == nullptr)
+	{
+		return btVector3(0.0, 0.0, 0.0);
+	}
+	else
+	{
+		btVector3 base = rigid_body->getLinearVelocity();
+		if (in_group != nullptr)
+		{
+			// We have to add the linear component that we may have
+			// got from rotation
+			btVector3 r = get_current_transform().getOrigin() - rigid_body->getWorldTransform().getOrigin();
+			btVector3 linear = rigid_body->getAngularVelocity().cross(r);
+			base += linear;
+		}
+
+		return base;
+	}
+}
+
+btVector3 Piece::get_current_angular()
+{
+	if (rigid_body == nullptr)
+	{
+		return btVector3(0.0, 0.0, 0.0);
+	}
+	else
+	{
+		return rigid_body->getAngularVelocity();
+		/*btVector3 rv = get_relative_position();
+		btVector3 pv = mass * (get_current_linear() - rigid_body->getLinearVelocity());
+
+		return rv.cross(pv) / mass;*/
+	}
+}
+
+btVector3 Piece::get_relative_position()
+{
+	return get_current_transform().getOrigin() - rigid_body->getWorldTransform().getOrigin();
 }
 
 Piece::Piece()
