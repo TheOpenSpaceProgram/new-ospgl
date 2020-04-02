955d42ea3185a8faa54e3308de273dea3c5627e5
Tatjam <tajampi@gmail.com>
2020-04-02 17:42:12 +0200

    Small code fix

--- src/vehicle/VehicleLoader.cpp
+++ src/vehicle/VehicleLoader.cpp
@@ -22,7 +22,7 @@ Vehicle* VehicleLoader::load_vehicle(cpptoml::table& root)
 		Part* n_part = new Part(part_proto, *part);
 
 		n_part->id = *part->get_qualified_as<int64_t>("id");
-		logger->check_important(n_part->id > n_vehicle->part_id, "Malformed vehicle, part ID too big");
+		logger->check_important(n_part->id <= n_vehicle->part_id, "Malformed vehicle, part ID too big");
 
 		n_vehicle->parts.push_back(n_part);
 		parts_by_id[n_part->id] = n_part;
