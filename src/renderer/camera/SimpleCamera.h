31bd84e52d954683671211ff16ce8702bdb87312
Damian Obernikowicz <damian.obernikowicz@gmail.com>
2020-06-08 22:42:03 +0200

    missing changes

--- src/renderer/camera/SimpleCamera.h
+++ src/renderer/camera/SimpleCamera.h
@@ -2,23 +2,27 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <tuple>
+#include <utility>
+#include <unordered_map>
 #include "CameraUniforms.h"
 #include "Camera.h"
+typedef struct GLFWwindow GLFWwindow;
 
-#define FREE_MODE 0
-#define CIRCLE_MODE 1
+#define CAMERA_FREE_MODE 0
+#define CAMERA_CIRCLE_MODE 1
 
 class SimpleCamera : public Camera
 {
 private:
+        static SimpleCamera *instance;
+
 	glm::dmat4 get_proj_view(int w, int h);
 	glm::dmat4 get_cmodel();
-
 public:
 
 	constexpr static double NEAR_PLANE = 1e-6;
 
-	bool keyboard_blocked;
+        bool keyboard_blocked;
 	unsigned char mode;
 
 	// In degrees
@@ -26,12 +30,14 @@ public:
 
 	double speed;
 
+	double distance;
 
 	glm::dvec3 pos;
 	glm::dvec3 up;
 	glm::dvec3 fw;
 
 	glm::dvec3 center;
+	glm::dvec3 center_fw;
 
 	void update(double dt);
 
@@ -39,63 +45,17 @@ public:
 	std::pair<glm::dvec3, glm::dvec3> get_camera_pos_dir();
 
 	virtual CameraUniforms get_camera_uniforms(int w, int h) override;
-
-	void forwards(double dt)
-	{
-		pos += speed * glm::normalize(fw) * (double)dt;
-	}
-
-	void backwards(double dt)
-	{
-		pos -= speed * glm::normalize(fw) * (double)dt;
-	}
-
-	void leftwards(double dt)
-	{
-		pos -= speed * glm::normalize(glm::cross(fw, up)) * (double)dt;
-	}
-
-	void rightwards(double dt)
-	{
-		pos += speed * glm::normalize(glm::cross(fw, up)) * (double)dt;
-	}
-
-	void tilt(double dt, double dir)
-	{
-		glm::mat4 f = glm::rotate(glm::dmat4(1.0), (double)dir * (double)dt, fw);
-		up = f * glm::dvec4(up, 1.0);
-	}
-
-	void upwards(double dt)
-	{
-		pos += speed * glm::normalize(up) * (double)dt;
-	}
-
-	void downwards(double dt)
-	{
-		pos -= speed * glm::normalize(up) * (double)dt;
-	}
-
-	void mouse(glm::dvec2 deltas, double dt)
-	{
-
-		glm::dvec3 or_forward = fw;
-		glm::dvec3 or_up = up;
-
-		// Rotate forward original up
-		glm::mat4 hor = glm::rotate(glm::dmat4(1.0), -deltas.x * (double)dt * 0.45, or_up);
-
-		glm::dvec3 right = glm::cross(or_forward, or_up);
-
-		glm::mat4 vert = glm::rotate(glm::dmat4(1.0), -deltas.y * (double)dt * 0.45, right);
-		up = vert * glm::dvec4(or_up, 1.0);
-		fw = vert * hor * glm::dvec4(fw, 1.0);
-
-	}
-
+        void forwards(double dt);
+        void backwards(double dt);
+        void leftwards(double dt);
+        void rightwards(double dt);
+        void tilt(double dt, double dir);
+        void upwards(double dt);
+        void downwards(double dt);
+	void toSphere();
+	void mouse(glm::dvec2 deltas, double dt);
 	void set_mode(unsigned char mode);
-	void init();
-
+	void init(glm::dvec3 up);
 	SimpleCamera();
-
+        void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 };
