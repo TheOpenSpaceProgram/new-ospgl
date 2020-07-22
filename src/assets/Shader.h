ee620e460f362a1e504d4f0493e934b8e57e1779
Tatjam <tajampi@gmail.com>
2020-07-22 15:23:29 +0200

    Small optimization to Shader

--- src/assets/Shader.h
+++ src/assets/Shader.h
@@ -27,50 +27,69 @@ private:
 	std::string preprocessor(const std::string& file);
 
 
+	// Decent improvement on some drivers as for some reason the glGetUniformLocation
+	// stalls the whole thing
+	std::unordered_map<std::string, GLint> uniform_locations;
 
 public:
 
 	GLuint id;
 
+	inline GLint get_uniform_location(const std::string& name)
+	{
+		auto it = uniform_locations.find(name);
+		if(it == uniform_locations.end())
+		{
+			GLint n_pos = glGetUniformLocation(id, name.c_str());
+			uniform_locations[name] = n_pos;
+			return n_pos;
+		}
+		else
+		{
+			return it->second;	
+		}
+		
+	}
+
 	void use();
 
-	inline void setBool(const std::string &name, bool value) const
+	inline void setBool(const std::string &name, bool value)
 	{
-		glUniform1i(glGetUniformLocation(id, name.c_str()), (int)value);
+		glUniform1i(get_uniform_location(name), (int)value);
 	}
-	inline void setInt(const std::string &name, int value) const
+	inline void setInt(const std::string &name, int value)
 	{
-		glUniform1i(glGetUniformLocation(id, name.c_str()), value);
-	}
-
-	inline void setFloat(const std::string &name, float value) const
-	{
-		glUniform1f(glGetUniformLocation(id, name.c_str()), value);
+		glUniform1i(get_uniform_location(name), value);
 	}
 
-	inline void setVec2(const std::string& name, glm::vec2 value) const
+	inline void setFloat(const std::string &name, float value)
 	{
-		glUniform2f(glGetUniformLocation(id, name.c_str()), value.x, value.y);
+		glUniform1f(get_uniform_location(name), value);
 	}
 
-	inline void setVec3(const std::string& name, glm::vec3 value) const
+	inline void setVec2(const std::string& name, glm::vec2 value)
 	{
-		glUniform3f(glGetUniformLocation(id, name.c_str()), value.x, value.y, value.z);
+		glUniform2f(get_uniform_location(name), value.x, value.y);
 	}
 
-	inline void setVec4(const std::string& name, glm::vec4 value) const
+	inline void setVec3(const std::string& name, glm::vec3 value)
 	{
-		glUniform4f(glGetUniformLocation(id, name.c_str()), value.x, value.y, value.z, value.w);
+		glUniform3f(get_uniform_location(name), value.x, value.y, value.z);
 	}
 
-	inline void setMat4(const std::string& name, glm::mat4 value) const
+	inline void setVec4(const std::string& name, glm::vec4 value)
 	{
-		glUniformMatrix4fv(glGetUniformLocation(id, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
+		glUniform4f(get_uniform_location(name), value.x, value.y, value.z, value.w);
 	}
 
-	inline void setMat3(const std::string& name, glm::mat3 value) const
+	inline void setMat4(const std::string& name, glm::mat4 value)
 	{
-		glUniformMatrix3fv(glGetUniformLocation(id, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
+		glUniformMatrix4fv(get_uniform_location(name), 1, GL_FALSE, glm::value_ptr(value));
+	}
+
+	inline void setMat3(const std::string& name, glm::mat3 value)
+	{
+		glUniformMatrix3fv(get_uniform_location(name), 1, GL_FALSE, glm::value_ptr(value));
 	}
 
 	Shader(const std::string& vertexData, const std::string& fragmentData, const std::string& pkg, const std::string& path);
