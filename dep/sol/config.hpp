62a2bc6b00c53f2f4777017940bcd7e0922bc5bc
drawblank <drawblank@protonmail.com>
2023-02-23 01:32:48 +1100

    updated sol library and config

--- dep/sol/config.hpp
+++ dep/sol/config.hpp
@@ -20,16 +20,14 @@
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 // This file was generated with a script.
-// Generated 2020-09-09 00:46:22.609996 UTC
-// This header was generated with sol v3.2.1 (revision e4f588d)
+// Generated 2022-06-25 08:14:19.336233 UTC
+// This header was generated with sol v3.3.0 (revision eba86625)
 // https://github.com/ThePhD/sol2
 
 #ifndef SOL_SINGLE_CONFIG_HPP
 #define SOL_SINGLE_CONFIG_HPP
 
 // beginning of sol/config.hpp
-
-#define SOL_LUAJIT 1
 
 /* Base, empty configuration file!
 
